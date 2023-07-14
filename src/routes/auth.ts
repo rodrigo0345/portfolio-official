1f7d84a9b00c22e8da1d1eecf6998ab6940ff313
rodrigo casanova <rodrigocralha@gmail.com>
2023-07-14 17:59:55 +0100

    everything is up and running :)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,9 +3,12 @@ import postRegister from '../controllers/auth/postRegister';
 import getUser from '../controllers/auth/getUser';
 import protectRoute from '../common/protect_route';
 import passport from 'passport';
+import { ApiSuccess } from '../common/api_response';
 
 export const authRouter = Router();
 
 authRouter.post('/register', postRegister);
-authRouter.post('/login', passport.authenticate('local'));
+authRouter.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json(ApiSuccess('Logged in'));
+});
 authRouter.get('/user', protectRoute, getUser);
