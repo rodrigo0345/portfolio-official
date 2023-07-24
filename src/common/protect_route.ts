aaad71ca99eb1c44a7c4c6de1898b72542b57a7a
rodrigo0345 <rodrigocralha@gmail.com>
2023-07-24 18:04:34 +0100

    adding authorization flash messages

--- src/common/protect_route.ts
+++ src/common/protect_route.ts
@@ -1,6 +1,7 @@
 import { adminEmail } from '..';
 import { ApiError } from './api_response';
 import dev_log from './dev_log';
+import { flash } from './flash';
 
 export default function protectRoute(req: any, res: any, next: any) {
   dev_log({ user: req.user, email: adminEmail });
@@ -8,5 +9,7 @@ export default function protectRoute(req: any, res: any, next: any) {
   if (req.isAuthenticated() && req.user.email === "rodrigocralha@gmail.com") {
     return next();
   }
+
+  flash('message', 'You are not authorized', res);
   return res.redirect('/blog');
 }
